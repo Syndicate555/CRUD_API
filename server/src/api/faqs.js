fdb367347ed7c1260cf928c59f0167b416437398
Syndicate555 <saziz028@uottawa.ca>
2020-08-25 22:49:48 -0400

    Create faqs.js

---
+++ server/src/api/faqs.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// Read
+router.get("/", (req, res, next) => {});
+
+// Create
+router.post("/", (req, res, next) => {});
+
+// Update
+router.put("/", (req, res, next) => {});
+
+//Delete
+router.delete("/", (req, res, next) => {});
