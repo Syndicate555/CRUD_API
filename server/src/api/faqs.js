15a450ab01e4d84339961212104725fcc056731c
Syndicate555 <saziz028@uottawa.ca>
2020-08-26 02:08:05 -0400

    changes

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -2,14 +2,20 @@ const express = require("express");
 const router = express.Router();
 const monk = require("monk");
 const config = require("config");
-const c = require("config");
 const db = config.get("mongoURI");
+const mongoose = require("mongoose");
 const connectDB = () => {
-  mongoose.connect(db, {
-    useNewURLParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
+  mongoose
+    .connect(db, {
+      useNewURLParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("mongoDB connected"))
+    .catch((err) => {
+      console.error(err.message);
+      process.exit(1);
+    });
 };
 
 const faqs = db.get("faqs");
