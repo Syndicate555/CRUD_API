7d079c1355e7bf56c7a24a426a2e2e0a0e1b24b1
Syndicate555 <saziz028@uottawa.ca>
2020-08-26 01:10:33 -0400

    Update faqs.js

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {});
+router.get("/", (req, res, next) => {
+  res.json({
+    message: "Hi read all",
+  });
+});
 router.post("/:id", (req, res, next) => {});
 router.put("/:id", (req, res, next) => {});
 router.delete("/:id", (req, res, next) => {});
